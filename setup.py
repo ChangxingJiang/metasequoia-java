004ae73ccbcdd7faf29db243564e9b0f16569843
changxing <1278729001@qq.com>
2025-09-17 08:37:50 +0800

    Java 解析器｜更新版本号（0.2.1）

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="UTF-8") as file:
 
 setup(
     name="metasequoia-java",
-    version="0.2.0",
+    version="0.2.1",
     description="水杉 Java 解析器",
     long_description=long_description,
     long_description_content_type="text/markdown",
